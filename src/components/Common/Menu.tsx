0117995111e8f73302a671b67ba2b5798b4b04e2
kimHyeonJu <kimhyesom1202@gmail.com>
2026-05-08 15:58:13 +0900

    Feature : Menu 컴포넌트 구현 및 메인 페이지에 적용

---
+++ src/components/Common/Menu.tsx
@@ -0,0 +1,124 @@
+import styled from '@emotion/styled';
+import { Search, Menu as MenuIcon } from 'lucide-react';
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  padding: 0 20px;
+  background-color: #fff;
+  z-index: 100;
+
+  @media (min-width: 768px) {
+    max-width: 768px;
+    height: 60px;
+    margin: 0 auto;
+  }
+`;
+
+const Logo = styled.h1`
+  margin: 0;
+
+  a {
+    display: flex;
+  }
+
+  img {
+    width: 40px;
+    height: 40px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+`;
+
+const MenuList = styled.ul`
+  display: none;
+  gap: 40px;
+  margin: 0;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    display: flex;
+  }
+`;
+
+const MenuItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a,
+  button {
+    position: relative;
+    font-size: 20px;
+    gap: 2px;
+
+    &:hover {
+      text-shadow: 0 0 0.5px currentColor;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: -2px;
+      left: 0;
+      width: 0;
+      height: 2px;
+      background-color: currentColor;
+      transition: width 0.2s ease;
+    }
+
+    &:hover::after {
+      width: 100%;
+    }
+  }
+`;
+
+const MobileMenuIcon = styled.button`
+  display: flex;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export default function Menu() {
+  return (
+    <Nav>
+      <Logo>
+        <a href="/">
+          <img src="" alt="logo" />
+        </a>
+      </Logo>
+
+      <MobileMenuIcon>
+        <MenuIcon />
+      </MobileMenuIcon>
+
+      <MenuList>
+        <MenuItem>
+          <a href="/about">about</a>
+        </MenuItem>
+        <MenuItem>
+          <a href="/posts">posts</a>
+        </MenuItem>
+        <MenuItem>
+          <a href="/portfolio">portfolio</a>
+        </MenuItem>
+        <MenuItem>
+          <button type="button">
+            <Search size={20} />
+            search
+          </button>
+        </MenuItem>
+      </MenuList>
+    </Nav>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
 import styled from '@emotion/styled';
 import GlobalStyle from '../components/Common/GlobalStyle';
+import Menu from '../components/Common/Menu';
 import Footer from '../components/Common/Footer';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   width: 100%;
   height: 100%;
+  padding-top: 40px;
+
+  @media (min-width: 768px) {
+    max-width: 768px;
+    padding-top: 60px;
+    margin: 0 auto;
+  }
 `;
 
 export default function IndexPage() {
   return (
     <Container>
       <GlobalStyle />
+      <Menu />
       <Footer />
     </Container>
   );
