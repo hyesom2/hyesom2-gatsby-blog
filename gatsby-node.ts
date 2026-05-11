205f2de2255a8c5cf3270932b7bedf6df91e7eee
kimHyeonJu <kimhyesom1202@gmail.com>
2026-05-11 16:33:21 +0900

    Refactor : @type, @contents path alias 추가

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -10,6 +10,8 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
         '@components': path.resolve(__dirname, 'src/components'),
         '@pages': path.resolve(__dirname, 'src/pages'),
         '@images': path.resolve(__dirname, 'src/images'),
+        '@type': path.resolve(__dirname, 'src/types'),
+        '@contents': path.resolve(__dirname, 'contents'),
       },
     },
   });
