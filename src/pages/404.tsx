c87d53426efb5145ccc7123e5a10d745ee348d6d
kimHyeonJu <kimhyesom1202@gmail.com>
2026-05-12 15:51:19 +0900

    Feature : NotFound 페이지 제작

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,53 @@
-import { HeadFC, Link, PageProps } from 'gatsby';
-import * as React from 'react';
+import GlobalStyle from '@components/Common/GlobalStyle';
+import styled from '@emotion/styled';
+import notFoundImg from '@images/404.png';
+import { Link } from 'gatsby';
 
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
+const NotFoundPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
+const NotFoundContainer = styled.div`
+  position: relative;
+`;
 
-const NotFoundPage: React.FC<PageProps> = () => {
+const NotFoundImage = styled.img`
+  width: 100%;
+
+  @media (min-width: 768px) {
+    width: 800px;
+  }
+`;
+
+const GoMainButton = styled(Link)`
+  position: absolute;
+  left: 50%;
+  bottom: 10%;
+  transform: translateX(-50%);
+  font-size: 16px;
+  color: var(--color-white);
+  background-color: var(--color-yellow);
+  padding: 10px 20px;
+  border: 1px solid var(--color-yellow);
+  border-radius: 24px;
+
+  @media (min-width: 768px) {
+    bottom: 15%;
+  }
+`;
+
+export default function NotFoundPage() {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <NotFoundPageWrapper>
+      <GlobalStyle />
+      <NotFoundContainer>
+        <NotFoundImage src={notFoundImg} alt="404 Not Found" />
+        <GoMainButton to="/">메인으로 돌아가기</GoMainButton>
+      </NotFoundContainer>
+    </NotFoundPageWrapper>
   );
-};
-
-export default NotFoundPage;
-
-export const Head: HeadFC = () => <title>Not found</title>;
+}
