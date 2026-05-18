877adec97fb63de2b12fd46ac868e54342dcd9f7
kimHyeonJu <kimhyesom1202@gmail.com>
2026-05-18 22:41:18 +0900

    Style : PostItem 스타일 수정

--- src/components/Main/PostItem.tsx
+++ src/components/Main/PostItem.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import ProfileImg from '@images/logo.png';
 import { Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import type { PostFrontmatterType } from 'src/types/PostItem.types';
@@ -9,27 +10,28 @@ const PostItemWrapper = styled(Link)`
   cursor: pointer;
   border: 1px solid #eee;
   border-radius: 10px;
+  padding: 20px;
   overflow: hidden;
+  gap: 20px;
+`;
+
+const PostItemHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
 `;
 
 const ThumbnailImage = styled(GatsbyImage)`
   width: 100%;
   height: 200px;
-  border-radius: 10px 10px 0 0;
+  border-radius: 8px;
   object-fit: cover;
-`;
-
-const PostItemContent = styled.div`
-  position: relative;
-  padding: 8px;
-
-  &:hover {
-    font-weight: 700;
-  }
+  margin-bottom: 20px;
 `;
 
 const Title = styled.h2`
-  display: -webkit-box;
+  display: block;
   text-overflow: ellipsis;
   white-space: normal;
   overflow: hidden;
@@ -37,30 +39,62 @@ const Title = styled.h2`
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   font-size: 16px;
-  font-weight: 400;
+  font-weight: 700;
+  margin-bottom: 8px;
+
+  @media (min-width: 768px) {
+    font-size: 20px;
+  }
+`;
+
+const Category = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
+`;
+
+const CategoryItem = styled.li`
+  font-size: 12px;
+  font-weight: 700;
+  color: var(--color-white);
+  background-color: var(--color-black);
+  border-radius: 4px;
+  padding: 4px 6px;
+`;
+
+const PostItemContent = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 16px;
+
+  &:hover {
+    font-weight: 700;
+  }
+`;
+
+const ProfileImage = styled.img`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  border: 1px solid var(--color-gray);
+  border-radius: 50%;
+`;
+
+const PostItemEtc = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  gap: 4px;
 `;
 
 const Date = styled.span`
   font-size: 14px;
   font-weight: 400;
   color: gray;
-`;
-
-const Category = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 10px;
-  margin: 10px -5px;
-`;
-
-const CategoryItem = styled.li`
-  font-size: 14px;
-  font-weight: 700;
-  color: #fff;
-  background-color: #000;
-  border-radius: 4px;
-  padding: 3px 5px;
-  margin: 2.5px 5px;
 `;
 
 const Summary = styled.p`
@@ -87,17 +121,24 @@ export default function PostItem({
 }: PostFrontmatterType) {
   return (
     <PostItemWrapper to={link || '#'}>
-      <ThumbnailImage image={gatsbyImageData} alt={title} />
-
-      <PostItemContent>
+      <PostItemHeader>
+        <ThumbnailImage image={gatsbyImageData} alt={title} />
         <Title>{title}</Title>
-        <Date>{date}</Date>
         <Category>
           {categories.map(category => (
             <CategoryItem key={category}>{category}</CategoryItem>
           ))}
         </Category>
-        <Summary>{summary}</Summary>
+      </PostItemHeader>
+
+      <Summary>{summary}</Summary>
+
+      <PostItemContent>
+        <ProfileImage src={ProfileImg} alt="작성자 프로필 이미지" />
+        <PostItemEtc>
+          <span>혜솜2</span>
+          <Date>{date}</Date>
+        </PostItemEtc>
       </PostItemContent>
     </PostItemWrapper>
   );
