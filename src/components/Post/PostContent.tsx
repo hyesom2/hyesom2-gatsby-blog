855a9aa643dbed0b9dfb5e7f554ae07c5bec80e8
kimHyeonJu <kimhyesom1202@gmail.com>
2026-05-30 19:08:49 +0900

    Style : Markdown 커스텀 스타일 변경(모바일 반응형)

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -8,14 +8,14 @@ const MarkdownRenderer = styled.div`
   // Renderer Style
   display: flex;
   flex-direction: column;
-  width: 768px;
+  width: 100%;
   margin: 0 auto;
-  padding: 100px 0;
+  padding: 80px 20px;
   word-break: break-all;
 
   // Markdown Style
   line-height: 1.8;
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 400;
 
   // Apply Padding Attribute to All Elements
@@ -44,15 +44,15 @@ const MarkdownRenderer = styled.div`
   }
 
   h1 {
-    font-size: 30px;
+    font-size: 24px;
   }
 
   h2 {
-    font-size: 25px;
+    font-size: 29px;
   }
 
   h3 {
-    font-size: 20px;
+    font-size: 16px;
   }
 
   // Adjust Quotation Element Style
@@ -75,7 +75,7 @@ const MarkdownRenderer = styled.div`
     border: 0;
     height: 1px;
     background-color: #000000;
-    margin: 40px 0;
+    margin: 30px 0;
   }
 
   // Adjust Link Element Style
@@ -99,6 +99,103 @@ const MarkdownRenderer = styled.div`
   code[class*='language-'],
   pre[class*='language-'] {
     tab-size: 2;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    // Renderer Style
+    padding: 100px 0;
+
+    // Markdown Style
+    line-height: 1.8;
+    font-size: 16px;
+    font-weight: 400;
+
+    // Apply Padding Attribute to All Elements
+    p {
+      padding: 3px 0;
+    }
+
+    // Adjust Heading Element Style
+    h1,
+    h2,
+    h3 {
+      font-weight: 800;
+      margin-bottom: 20px;
+    }
+
+    * + h1,
+    * + h2,
+    * + h3 {
+      margin-top: 40px;
+    }
+
+    hr + h1,
+    hr + h2,
+    hr + h3 {
+      margin: 0;
+    }
+
+    h1 {
+      font-size: 30px;
+    }
+
+    h2 {
+      font-size: 25px;
+    }
+
+    h3 {
+      font-size: 20px;
+    }
+
+    // Adjust Quotation Element Style
+    blockquote {
+      margin: 30px 0;
+      padding: 5px 15px;
+      border-left: 2px solid #000000;
+      font-weight: 800;
+    }
+
+    // Adjust List Element Style
+    ol,
+    ul {
+      margin-left: 20px;
+      padding: 30px 0;
+    }
+
+    // Adjust Horizontal Rule style
+    hr {
+      border: 0;
+      height: 1px;
+      background-color: #000000;
+      margin: 40px 0;
+    }
+
+    // Adjust Link Element Style
+    a {
+      color: #4263eb;
+      text-decoration: underline;
+    }
+
+    // Adjust Code Style
+    pre[class*='language-'] {
+      margin: 30px 0;
+      padding: 15px;
+      font-size: 15px;
+
+      ::-webkit-scrollbar-thumb {
+        background: rgba(255, 255, 255, 0.5);
+        border-radius: 3px;
+      }
+    }
+
+    code[class*='language-'],
+    pre[class*='language-'] {
+      tab-size: 2;
+    }
   }
 `;
 
